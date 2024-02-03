27f06c44adee7f58133fe987a7af461c82b8d39f
CaioSalgado1995 <caiosalgado1995@gmail.com>
2024-02-03 04:36:08 -0300

    (api) Removing js files

--- pmedicine-api/src/model/extractConfigType.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var ExtractConfigType;
-(function (ExtractConfigType) {
-    ExtractConfigType[ExtractConfigType["TEXT"] = 0] = "TEXT";
-    ExtractConfigType[ExtractConfigType["SCRIPT"] = 1] = "SCRIPT";
-    ExtractConfigType[ExtractConfigType["TABLE"] = 2] = "TABLE";
-})(ExtractConfigType || (ExtractConfigType = {}));
