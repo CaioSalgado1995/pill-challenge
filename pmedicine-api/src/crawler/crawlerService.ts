0fad1f969ac399d6cba7990a34ff891093944a70
CaioSalgado1995 <caiosalgado1995@gmail.com>
2024-02-04 10:48:30 -0300

    (api) Add documentation to the main methods

--- pmedicine-api/src/crawler/crawlerService.ts
+++ pmedicine-api/src/crawler/crawlerService.ts
@@ -27,6 +27,12 @@ function extractFrom(
     }
 }
 
+/**
+ * Evaluate the config to be extracted, executing the properly code to extract
+ * @param root the DOM root node
+ * @param extractConfigDetails details for each property, contaning the type and the custom properties if necessary
+ * @returns the value extracted
+ */
 function evaluate(root: cheerio.Root, extractConfigDetails: ExtractConfigDetails): string {
     switch(extractConfigDetails.type){
         case ExtractConfigType.TEXT:
@@ -45,7 +51,7 @@ function evaluate(root: cheerio.Root, extractConfigDetails: ExtractConfigDetails
 /**
  * Extracting the barcode from a DOM table
  * @param root the DOM root node
- * @param extractConfig a configuration that indicates how to extract the data from the html information
+ * @param extractConfigDetails a configuration that indicates how to extract the data from the html information
  * @returns 
  */
 function getFromTable(root: cheerio.Root, extractConfigDetails: ExtractConfigDetails) {
@@ -60,6 +66,12 @@ function getFromTable(root: cheerio.Root, extractConfigDetails: ExtractConfigDet
         .text()
 }
 
+/**
+ * Extracting the price from a DOM script
+ * @param root the DOM root node
+ * @param extractConfigDetails a configuration that indicates how to extract the data from the html information
+ * @returns 
+ */
 function getFromScript(root: cheerio.Root, extractConfigDetails: ExtractConfigDetails) {
     const customData = (extractConfigDetails.extraInfo as ExtraInfoScript)
     return lodash.get(JSON.parse(root(extractConfigDetails.selector).text()), customData.path)
